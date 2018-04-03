f1ac919406d6a5f63dbcea7553cd2168fd5e1ec1
xavier <xavier@MacBook-Pro-de-xavier.local>
2018-04-03 16:07:30 -0300

    modified:   src/main/java/com/keenvil/platori/domain/ErrorDto.java

--- src/main/java/com/keenvil/platori/domain/ErrorDto.java
+++ src/main/java/com/keenvil/platori/domain/ErrorDto.java
@@ -2,30 +2,13 @@ package com.keenvil.platori.domain;
 
 public class ErrorDto {
   
-  private int httpStatus;
-  
   private String code;
   
-  private String title;
-  
-  private String detail;
-  
   public ErrorDto() {
-  }
-  
-  public int getHttpStatus() {
-    return httpStatus;
   }
   
   public String getCode() {
     return code;
   }
   
-  public String getTitle() {
-    return title;
-  }
-  
-  public String getDetail() {
-    return detail;
-  }
 }
